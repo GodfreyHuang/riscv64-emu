58d404af5f651d04654a616c876a7bfbb246e14c
Godfrey Huang <joker103186@gmail.com>
2023-09-26 16:33:14 +0800

    update readme and cpu operation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 The practice of risc-v  
   
 Note: to use gdb debug, type like this:
-``` gcc -o main -Og -g main.c ```  
+``` gcc -o ./bin/main -Og -g main.c ```  
   
 Reference:  
 https://fmash16.github.io/content/posts/riscv-emulator-in-c.html
--- src/cpu.c
+++ src/cpu.c
@@ -7,6 +7,18 @@
 #include "dram.h"
 #include "opcode.h"
 
+#define ANSI_YELLOW "\x1b[33m"
+#define ANSI_BLUE "\x1b[31m"
+#define ANSI_RESET "\x1b[0m"
+
+// print operation for DEBUG
+void print_op(char *s)
+{
+    printf("%s%s%s", ANSI_BLUE, s, ANSI_RESET);
+}
+
+// ---------- Initialize ----------
+
 void cpu_init(CPU *cpu)
 {
     cpu->regs[0] = 0x00;
@@ -16,27 +28,34 @@ void cpu_init(CPU *cpu)
         DRAM_BASE;  // The program counter points to the start of the memory
 }
 
+// ---------- Instruction Decode ----------
+// the address of destination register
 uint64_t rd(uint32_t inst)
 {
     return (inst >> 7) & 0x1f;  // rd in bits 11..7
 }
 
+// the address of source register 1
 uint64_t rs1(uint32_t inst)
 {
     return (inst >> 15) & 0x1f;  // rs1 in bits 19..15
 }
 
+// the address of source register 2
 uint64_t rs2(uint32_t inst)
 {
     return (inst >> 20) & 0x1f;  // rs2 in bits 24..20
 }
 
+// A value which gives the address of destination register
+// I-Type: Immediate type instructions
 uint64_t imm_I(uint32_t inst)
 {
     // imm[11:0] = inst[31:20]
     return ((int64_t) (int32_t) (inst & 0xfff00000)) >> 20;
 }
 
+// S-Type: Store type instructions
 uint64_t imm_S(uint32_t inst)
 {
     // imm[11:5] = inst[31:25], imm[4:0] = inst[11:7]
@@ -44,6 +63,7 @@ uint64_t imm_S(uint32_t inst)
            ((inst >> 7) & 0x1f);
 }
 
+// B-Type: Break type instructions
 uint64_t imm_B(uint32_t inst)
 {
     // imm[12|10:5|4:1|11] = inst[31|30:25|11:8|7]
@@ -53,12 +73,14 @@ uint64_t imm_B(uint32_t inst)
            | ((inst >> 7) & 0x1e);   // imm[4:1]
 }
 
+// U-Type: Register type instructions
 uint64_t imm_U(uint32_t inst)
 {
     // imm[31:12] = inst[31:12]
     return (int64_t) (int32_t) (inst & 0xfffff999);
 }
 
+// J-Type: Jump type instructions
 uint64_t imm_J(uint32_t inst)
 {
     // imm[20|10:1|11|19:12] = inst[31|30:21|20|19:12]
@@ -68,6 +90,7 @@ uint64_t imm_J(uint32_t inst)
            | ((inst >> 20) & 0x7fe);  // imm[10:1]
 }
 
+// the shift amount
 uint32_t shamt(uint32_t inst)
 {
     // shamt(shift amount) only required for immediate shift instructions
@@ -146,18 +169,40 @@ int cpu_execute(CPU *cpu, uint32_t inst)
     }
 }
 
+void exec_ADD(CPU *cpu, uint32_t inst)
+{
+    cpu->regs[rd(inst)] = cpu->regs[rs1(inst)] + cpu->regs[rs2(inst)];
+    print_op("add\n");
+}
+
 void exec_ADDI(CPU *cpu, uint32_t inst)
 {
     uint64_t imm = imm_I(inst);
     cpu->regs[rd(inst)] = cpu->regs[rs1(inst)] + (int64_t) imm;
     print_op("addi\n");
 }
+
+void exec_SLT(CPU *cpu, uint32_t inst)
+{
+    cpu->regs[rd(inst)] =
+        (cpu->regs[rs1(inst)] < (int64_t) cpu->regs[rs2(inst)]) ? 1 : 0;
+    print_op("slt\n");
+}
+
 void exec_SLTI(CPU *cpu, uint32_t inst)
 {
     uint64_t imm = imm_I(inst);
     cpu->regs[rd(inst)] = (cpu->regs[rs1(inst)] < (int64_t) imm) ? 1 : 0;
     print_op("slti\n");
 }
+
+void exec_SRA(CPU *cpu, uint32_t inst)
+{
+    cpu->regs[rd(inst)] =
+        (int32_t) cpu->regs[rs1(inst)] >> (int64_t) cpu->regs[rs2(inst)];
+    print_op("sra\n");
+}
+
 void exec_SRAI(CPU *cpu, uint32_t inst)
 {
     uint64_t imm = imm_I(inst);
